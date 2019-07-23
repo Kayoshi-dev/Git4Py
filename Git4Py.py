e4548c3254a5c3ac5ebfce4dc58431b6f3f1d76b
Maxime JAUME <maxime.jme@gmail.com>
2019-07-23 20:13:09 +0200

    Display new status, pull when the folder already exist

--- Git4Py.py
+++ Git4Py.py
@@ -3,29 +3,51 @@
 
 #Simple Python 3 script to interact with a Github repository
 
-import subprocess
+import subprocess, os, glob, io
 
 urlRepo = "https://github.com/Kayoshi-dev/SimpleOOPDashboard"
+dirname = urlRepo.split('/')[4]
 
-print("""
+def gitMain():
+    print("""
 
-Welcome to :
-   ______   _   _    _    _    _______         
- .' ___  | (_) / |_ | |  | |  |_   __ \        
-/ .'   \_| __ `| |-'| |__| |_   | |__) |_   __ 
-| |   ____[  | | |  |____   _|  |  ___/[ \ [  ]
-\ `.___]  || | | |,     _| |_  _| |_    \ '/ / 
- `._____.'[___]\__/    |_____||_____| [\_:  /  
-                                       \__.'   
-""")
+    Welcome to :
+       ______   _   _    _    _    _______         
+     .' ___  | (_) / |_ | |  | |  |_   __ \        
+    / .'   \_| __ `| |-'| |__| |_   | |__) |_   __ 
+    | |   ____[  | | |  |____   _|  |  ___/[ \ [  ]
+    \ `.___]  || | | |,     _| |_  _| |_    \ '/ / 
+     `._____.'[___]\__/    |_____||_____| [\_:  /  
+                                           \__.'   
+    """)
+
+    gitClone()
 
 #This function clone a repository
 def gitClone():
-    try:
-        subprocess.check_call(['git', 'clone', urlRepo])
-        print('Downloading the Github repository...')
-        print('Finished!💪')
-    except subprocess.CalledProcessError as e:
-        print('An error has occurred : ', e.returncode)
+    if os.path.exists(os.getcwd() + os.sep + dirname):
+        os.chdir(dirname)
+        try:
+            pullResult = subprocess.Popen(['git', 'pull'], stdout=subprocess.PIPE)
+            print('Updating...⌛')
+            while True:
+                line = pullResult.stdout.readline()
+                if not line:
+                    break
+                print(line)
+        except subprocess.CalledProcessError as e:
+            print('❌ An error has occurred : ', e.returncode)
+    else:
+        try:
+            cloneResult = subprocess.Popen(['git', 'clone', urlRepo], stdout=subprocess.PIPE)
+            print('Downloading the Github repository...⌛')
+            while True:
+                line = cloneResult.stdout.readline()
+                if not line:
+                    break
+                print(line)
+            print('Finished!💪')
+        except subprocess.CalledProcessError as e:
+            print('❌ An error has occurred : ', e.returncode)
 
-gitClone()
+gitMain()
